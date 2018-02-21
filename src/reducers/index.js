40b543250db8414bfe0778eea2dea55fb141865b
Zankhana Rana <zankhanaranauh@gmail.com>
2018-02-21 01:41:22 +0000

    Update src

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import PostsReducer from './reducer_post';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  form: formReducers
+  form: formReducer
 });
 
 export default rootReducer;
