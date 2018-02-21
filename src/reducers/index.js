13b809b818fddb29e938f0d195bd6f673c9e3838
Zankhana Rana <zankhanaranauh@gmail.com>
2018-02-21 01:40:48 +0000

    Update src

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { routerStateReducer } from 'redux-router';
 import PostsReducer from './reducer_post';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  form: formReducer,
-  router: routerStateReducer
+  form: formReducers
 });
 
 export default rootReducer;
