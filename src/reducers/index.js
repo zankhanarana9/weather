87aa99e0c997b434d148f220ce151bc8a71b1f42
Zankhana Rana <zankhanaranauh@gmail.com>
2018-02-21 01:39:06 +0000

    ek page baki h

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer, routerStateReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
+import { routerStateReducer } from 'redux-router';
 import PostsReducer from './reducer_post';
 
 const rootReducer = combineReducers({
